30ba7aaea57eeee8668493035c3b17308677e8e3
8bit-euclid <8bit.euclid@gmail.com>
2024-06-08 23:00:32 -0400

    Static SegmentChain working. Yet to resurrect intro animation.

--- line.py
+++ line.py
@@ -1,12 +1,15 @@
-from segment import Segment
+from segment import *
 from general import *
 
 
 clear_scene()
 set_render_fps(30)
 
-line_seg = Segment(vert0=(0.0, 0.0), vert1=(5.0, 0.0), thickness=0.1, side_bias=0.0, angle_offs0=0,
-                   angle_offs1=0, intro=('00:00', '00:00:500'))
+# line_seg = Segment(vert0=(0.0, 0.0), vert1=(5.0, 0.0), width=0.1, bias=0.0, angle_offs0=0,
+#                    angle_offs1=0, intro=('00:00', '00:00:500'))
+
+segs = SegmentChain([(0.0, 0.0), (5.0, 0.0), (10.0, 2.0)], width=0.1, bias=0.0, angle_offs0=0,
+                    angle_offs1=0)
 
 # Set end frame
 set_end_frame(to_frame('00:03'))
