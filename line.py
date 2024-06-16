76149cfc2b992f692b9a9dd960b4f10ef8306266
8bit-euclid <8bit.euclid@gmail.com>
2024-06-16 13:51:54 -0400

    Fixed sequential interpolation of multiple segments. Yet to fix 'hiding' subsequent vertices.

--- line.py
+++ line.py
@@ -8,8 +8,8 @@ set_render_fps(30)
 # line_seg = Segment(vert0=(0.0, 0.0), vert1=(5.0, 0.0), width=0.1, bias=0.0, angle_offs0=0,
 #                    angle_offs1=0, intro=('00:00', '00:00:500'))
 
-segs = SegmentChain([(0.0, 0.0), (5.0, 0.0), (10.0, 2.0)], width=0.1, bias=0.0, angle_offs0=0,
-                    angle_offs1=0)
+segs = SegmentChain([(0.0, 0.0), (5.0, 0.0), (10.0, 2.0), (15.0, 2.0)], width=0.1, bias=0.0, angle_offs0=0,
+                    angle_offs1=0, intro=('00:00', '00:01:500'))
 
 # Set end frame
 set_end_frame(to_frame('00:03'))
