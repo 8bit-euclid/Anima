a7902ff60251f9cbd9e1e0c9c7757aab544824d1
NiranAGMT <niran@augmenta.ai>
2024-06-16 13:51:54 -0400

    Fixed sequential interpolation of multiple segments. Yet to fix 'hiding' subsequent vertices.

--- segment.py
+++ segment.py
@@ -10,29 +10,31 @@ class SegmentChain:
         self.name = "Segment"
         self.obj = None
         self.dim = dimension
+
         # Note: currently only works for 2D but we resize to 3.
         self.vertices = [Vector(v).resized(3) for v in vertices]
         self.angle_offs0 = angle_offs0
         self.angle_offs1 = angle_offs1
         self.width = width
         assert -1.0 <= bias <= 1.0, \
-            "The width bias must be in range [-1.0, 1.0]."
+            "The width bias must be in the range [-1.0, 1.0]."
         self.bias = bias
+        self.segment_lengths = []
         self.total_length = 0.0
-        self.intro = intro
-        self.outro = outro
 
         # Build object
         self._build()
 
         # Create intro animation, if set.
-        if self.intro:
+        if intro:
+            self.intro = intro
             if not isinstance(intro, Interval):
                 self.intro = Interval(*intro)
             self._make_intro()
 
         # Create outro animation, if set.
-        if self.outro:
+        if outro:
+            self.outro = outro
             if not isinstance(outro, Interval):
                 self.outro = Interval(*outro)
             self._make_outro()
@@ -43,7 +45,6 @@ class SegmentChain:
         half_width = 0.5 * self.width
         bias = self.bias
         obj_verts = self.obj.data.vertices
-        total_length = self.total_length
 
         # Compute offset angles at each vertex.
         n_verts = len(verts)
@@ -58,13 +59,15 @@ class SegmentChain:
             angle_offs.append(-0.5 * tang_prev.angle_signed(tang_curr))
         angle_offs.append(self.angle_offs1)  # Last vertex.
 
-        total_length = 0.0
+        self.total_length = 0.0
 
         for i in range(n_verts):
             # Compute the normalised tangent of the segment and update total length.
             tang = verts[i] - verts[i - 1] if i != 0 else verts[1] - verts[0]
             assert tang.length > 0.0, "A segment must have non-zero length."
-            total_length += tang.length
+            if i != 0:
+                self.segment_lengths.append(tang.length)
+                self.total_length += tang.length
             tang = tang.normalized()
 
             # Compute the normal to the tangent.
@@ -83,7 +86,7 @@ class SegmentChain:
                 direc = direc / direc.dot(norm)
 
             # Position object vertices, considering end-directions, width, and bias.
-            j = 2 * i
+            j = 2 * i  # Object vertex index
             obj_verts[j + 0].co = verts[i] + (bias - 1.0) * half_width * direc
             obj_verts[j + 1].co = verts[i] + (bias + 1.0) * half_width * direc
 
@@ -94,11 +97,11 @@ class SegmentChain:
         obj_faces = []
         for i in range(n_verts):
             for _ in range(2):
-                obj_verts.append((0., 0., 0.))
+                obj_verts.append((0, 0, 0))
             if i < n_verts - 1:
-                k = 2*i
-                obj_faces.append((k+0, k+1, k+2))
-                obj_faces.append((k+1, k+3, k+2))
+                k = 2 * i  # Object vertex index
+                obj_faces.append((k + 0, k + 1, k + 2))
+                obj_faces.append((k + 1, k + 3, k + 2))
 
         # Create new bpy mesh and update base object.
         mesh = create_mesh("mesh", obj_verts, obj_faces)
@@ -108,7 +111,7 @@ class SegmentChain:
         assert self.intro, "The intro animation interval was not set."
         assert self.obj, "The blender object has not been initialised."
 
-        # Set load factor keyframes.
+        # Set load ratio keyframes.
         self.obj["load_ratio"] = 0.0
         self.obj.keyframe_insert(
             data_path='["load_ratio"]', frame=to_frame(self.intro.start))
@@ -117,18 +120,31 @@ class SegmentChain:
             data_path='["load_ratio"]', frame=to_frame(self.intro.stop))
 
         # Set vertices 2 and 3 to be driven by the 'load_ratio'.
-        for i in [2, 3]:
+        total_len = self.total_length
+        cumu_seg_len = 0.0
+        for i in range(1, len(self.vertices)):
             obj_verts = self.obj.data.vertices
-            vert0 = obj_verts[i - 2].co
-            vert1 = obj_verts[i].co
+            curr_seg_len = self.segment_lengths[i - 1]
 
-            # Get the drivers for the 3 vertex coordinates.
-            drivers = add_driver(obj_verts[i], "co")
-            for j, driver in enumerate(drivers):
-                # Create a linear expression based on the 'load_ratio'.
-                driver_expr = f"({vert0[j]} * (1 - t) + {vert1[j]} * t)"
-                add_driver_script(driver, self.obj,
-                                  '["load_ratio"]', 't', driver_expr)
+            a = cumu_seg_len / total_len
+            b = curr_seg_len / total_len
+
+            for j in [2*i, 2*i + 1]:
+                vert0 = obj_verts[j - 2].co
+                vert1 = obj_verts[j].co
+
+                # Get the drivers for the 3 vertex coordinates.
+                drivers = add_driver(obj_verts[j], "co")
+                for k, driver in enumerate(drivers):
+                    # Linearly interpolate between the two vertices based on the 'load_ratio'.
+                    driver_expr = f"0.0 if t < {a} else ({vert1[k]} if t > {a + b} else {vert0[k]} * (1 - (t - {a}) / {b}) + {vert1[k]} * (t - {a}) / {b})"
+
+                    # Add driver script.
+                    add_driver_script(driver, self.obj,
+                                      '["load_ratio"]', 't', driver_expr)
+
+            # Update cumulative segment length.
+            cumu_seg_len += curr_seg_len
 
     def _make_outro(self):
         assert self.outro, "The outro animation interval was not set."
