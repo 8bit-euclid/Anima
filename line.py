8a5ff4a0580437afeb2266028df6276d61441a37
8bit-euclid <8bit.euclid@gmail.com>
2024-07-06 19:45:11 -0400

    Tracing of segment chains now working with shape keys.

--- line.py
+++ line.py
@@ -3,13 +3,16 @@ from general import *
 
 
 clear_scene()
-set_render_fps(30)
+set_render_fps(60)
 
 # line_seg = Segment(vert0=(0.0, 0.0), vert1=(5.0, 0.0), width=0.1, bias=0.0, angle_offs0=0,
-#                    angle_offs1=0, intro=('00:00', '00:00:500'))
+#                    angle_offs1=0, intro=('00:00', '00:02'), outro=('00:04', '00:06'))
 
-segs = SegmentChain([(0.0, 0.0), (5.0, 0.0), (10.0, 2.0), (15.0, 2.0)], width=0.1, bias=0.0, angle_offs0=0,
-                    angle_offs1=0, intro=('00:00', '00:02'), outro=('00:04', '00:06'))
+# segs = SegmentChain([(0.0, 0.0), (5.0, 0.0), (10.0, 2.0), (15.0, 2.0)], width=0.1, bias=0.0, angle_offs0=0,
+#                     angle_offs1=0, intro=('00:00', '00:02'), outro=('00:04', '00:06'))
+
+elli = Ellipse((0.0, 0.0), 1.0, 1.0, width=0.01, bias=0.0, angle_offs0=0,
+               angle_offs1=0, intro=('00:00', '00:02'), outro=('00:04', '00:06'))
 
 # Set end frame
 set_end_frame(to_frame('00:07'))
