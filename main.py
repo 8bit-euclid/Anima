ee6b53dac195fc80dd8dc85c7752ba740151f5ce
NiranAGMT <niran@augmenta.ai>
2024-07-07 21:43:40 -0400

    More cleaning up. Added elliptical/circular arc classes and derived ellipse and circle from them.

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ set_render_fps(60)
 # segs = SegmentChain([(0.0, 0.0), (5.0, 0.0), (10.0, 2.0), (15.0, 2.0)], width=0.1, bias=0.0, angle_offs0=0,
 #                     angle_offs1=0, intro=('00:00', '00:02'), outro=('00:04', '00:06'))
 
-elli = Ellipse((0.0, 0.0), 1.0, 1.0, width=0.01, bias=0.0, angle_offs0=0,
-               angle_offs1=0, intro=('00:00', '00:02'), outro=('00:04', '00:06'))
+# elli = Ellipse((0.0, 0.0), 1.0, 1.0, width=0.01, bias=0.0, angle_offs0=0,
+#                angle_offs1=0, intro=('00:00', '00:02'), outro=('00:04', '00:06'))
+
+obj = Circle((0.0, 0.0), 1.0, width=0.01, bias=0.0,
+             intro=('00:00', '00:02'), outro=('00:04', '00:06'))
 
 # Set end frame
 set_end_frame(to_frame('00:07'))
 
-save_as("line")
+save_as("main")
