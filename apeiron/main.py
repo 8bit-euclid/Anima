686afb67b0f5c052cba8e4b6469aafed7ea9cca0
8bit-euclid <8bit.euclid@gmail.com>
2024-10-23 13:38:24 -0400

    Cleanup

--- apeiron/main.py
+++ apeiron/main.py
@@ -10,30 +10,6 @@ startup.blender_startup.register()
 clear_scene()
 set_render_fps(60)
 
-#
-#
-#
-
-# line_seg = Segment(vert0=(0.0, 0.0), vert1=(5.0, 0.0), width=0.1, bias=0.0, angle_offs0=0,
-#                    angle_offs1=0, intro=('00:00', '00:02'), outro=('00:04', '00:06'))
-
-# segs = SegmentChain([(0.0, 0.0), (5.0, 0.0), (10.0, 2.0), (15.0, 2.0)], width=0.1, bias=0.0, angle_offs0=0,
-#                     angle_offs1=0, intro=('00:00', '00:02'), outro=('00:04', '00:06'))
-
-# elli = Ellipse((0.0, 0.0), 1.0, 1.0, width=0.01, bias=0.0, angle_offs0=0,
-#                angle_offs1=0, intro=('00:00', '00:02'), outro=('00:04', '00:06'))
-
-# obj = Circle((0.0, 0.0), 1.0, width=0.01, bias=0.0,
-#              intro=('00:00', '00:02'), outro=('00:04', '00:06'))
-
-# segs = SegmentChain([(0.0, 0.0), (5.0, 0.0), (5.0, 6.0)], width=0.1, bias=0.0, angle_offs0=0,
-# segs = SegmentChain([(0.0, 0.0), (5.0, 0.0), (10.0, 5.0)], width=0.1, bias=0.0, angle_offs0=0,
-#                     angle_offs1=0, intro=('00:00', '00:07'))
-
-#
-#
-#
-
 obj = Rectangle(1, 1)
 obj.animate()
 
