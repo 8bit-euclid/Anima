96e5b813281ed3707cef6368fffcab18600cdf20
8bit-euclid <8bit.euclid@gmail.com>
2024-10-25 13:30:34 -0400

    Refactored so that easybpy is imported as ebpy

--- apeiron/main.py
+++ apeiron/main.py
@@ -1,18 +1,16 @@
 from apeiron.primitives.curves import *
 from apeiron.globals.general import *
 import apeiron.startup as startup
-from apeiron.primitives.base import BaseObject, get_blender_object
-from apeiron.primitives.points import Empty, Point
 
 startup.driver_callables.copy_startup_files()
 startup.blender_startup.register()
 clear_scene()
-set_render_fps(60)
+ebpy.set_render_fps(60)
 
 obj = Rectangle(1, 1)
 obj.animate()
 
 # Set end frame
-set_end_frame(to_frame('00:07'))
+ebpy.set_end_frame(to_frame('00:07'))
 
 save_as("main")
