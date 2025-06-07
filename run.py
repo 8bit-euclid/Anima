b1bc5cb9850e75eb0f1825caa73996c07e885f4c
8bit-euclid <8bit-euclid@gmail.com>
2025-06-07 13:07:43 -0400

    Cleaned up run.py script

--- run.py
+++ run.py
@@ -2,20 +2,29 @@ import sys
 import os
 import importlib
 
-proj_name = 'anima'
+PROJECT_NAME = 'anima'
 
-# Add the project root directory to sys.path
-root_dir = os.path.dirname(os.path.abspath(__file__))
-if root_dir not in sys.path:
-    sys.path.append(root_dir)
 
-# Delete all anima-related modules (only applies when reloading)
-for module in list(sys.modules.keys()):
-    if proj_name in module:
-        del sys.modules[module]
+def configure_reload():
+    """Configure the script to auto-reload modules when changes are made."""
+    # Add the project root directory to sys.path
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    if root_dir not in sys.path:
+        sys.path.append(root_dir)
 
-# Load main module
-proj = importlib.import_module(proj_name + '.main')
+    # Delete all anima-related modules from sys.modules (only matters when reloading)
+    for module in list(sys.modules.keys()):
+        if PROJECT_NAME in module:
+            del sys.modules[module]
 
-# Run main routine
-proj.main()
+
+def run():
+    """Run the main module of the project."""
+    configure_reload()
+
+    # Load and run the main module
+    proj = importlib.import_module(PROJECT_NAME + '.main')
+    proj.main()
+
+
+run()
