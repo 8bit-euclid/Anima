5fabf7497b8cc61b2ca4223d5c5062aab2617f8a
8bit-euclid <8bit-euclid@gmail.com>
2025-06-12 22:31:03 -0400

    Fixed and cleaned up tests for BezierSpline, CurveChain, and Joint

--- src/anima/diagnostics/profiler.py
+++ src/anima/diagnostics/profiler.py
@@ -5,16 +5,31 @@ from pstats import SortKey
 
 
 class Profiler():
+    """
+    A simple wrapper around Python's cProfile to profile code execution.
+
+    Usage:
+        profiler = Profiler()
+        profiler.enable()
+        # ... code to profile ...
+        profiler.disable()
+        profiler.print_stats()
+    """
+
     def __init__(self):
+        """Initialize the Profiler instance and underlying cProfile.Profile object."""
         self._prof = cProfile.Profile()
 
     def enable(self):
+        """Start collecting profiling data."""
         self._prof.enable()
 
     def disable(self):
+        """Stop collecting profiling data."""
         self._prof.disable()
 
     def print_stats(self):
+        """Print the collected profiling statistics, sorted by internal time."""
         stream = io.StringIO()
         sortby = SortKey.TIME
         ps = pstats.Stats(self._prof, stream=stream).sort_stats(sortby)
