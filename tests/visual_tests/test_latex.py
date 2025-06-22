973eab3690545fafa46d37a624ccb3e43a15ccbd
8bit-euclid <8bit-euclid@gmail.com>
2025-06-22 01:54:32 -0400

    wip: Parse glyphs in latex string
    - Now parsing characters and symbols.
    - Addressed subtlety when node has 'attributes', e.g. \frac{a}{b}
    - Using logger to log debug output

--- tests/visual_tests/test_latex.py
+++ tests/visual_tests/test_latex.py
@@ -1,9 +1,31 @@
 from anima.latex.tex_document_processor import TeXDocumentProcessor
 from anima.latex.tex_document import TeXDocument
 from anima.latex.glyph import Glyph
+from anima.latex.tex_string_parser import TeXStringParser
 
 
 def test_text_to_glyphs():
+    # text = r"""
+    # \section*{Introduction}
+
+    # This is \textbf{bold font} text with math: $E ^ 2 = (\mathit{mc} ^ 2) ^ 2 + (pc) ^ 2$.
+
+    # A fraction: $\frac{a}{b}$
+
+    # \begin{itemize}
+    # \item[1] First item
+    # \item[2] Second item with \emph{emphasis}
+    # \end{itemize}
+
+    # Display math:
+    # $$\int_0 ^\infty e ^ {-x} dx = 1$$
+
+    # \begin{equation}
+    # \label{eq: example}
+    # a ^ 2 + b ^ 2 = c ^ 2
+    # \end{equation}
+    # """
+
     # text = r'Lyy\\y'
     # text = 'T'
     # text = 'S'
@@ -21,12 +43,16 @@ def test_text_to_glyphs():
     # \end{align}
     # """
     # text = '$E = mc^2$'
-    # text = r'$E^2 = (\mathit{mc}^2)^2 + (pc)^2$'
+    text = r'$E^2 = (\mathit{mc}^2)^2 + (pc)^2$'
     # text = r'$E = \mathit{mc}^2$'
     # text = 'C'
     # text = '8'
     # text = 'O{\Huge 8}'
     # text = 'O'
+    # text = r'$s = ut + \frac{1}{2}at^2$'
+    # text = r'$\frac{x^2 - 1}{x^2 + 1}$'
+
+    glyphs = TeXStringParser(text).parse()
 
     # Use provided latex document or setup a default one
     if isinstance(text, str):
@@ -37,4 +63,4 @@ def test_text_to_glyphs():
         for _, path in glyph_paths.items():
             glyph = Glyph(path)
 
-    print(*positions, sep='\n')
+    # print(*positions, sep='\n')
