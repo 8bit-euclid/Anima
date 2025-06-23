03c00b5c905420dffe1933bb6c48cc270f98c8a2
8bit-euclid <8bit-euclid@gmail.com>
2025-06-23 02:12:18 -0400

    chore: Set default log level to DEBUG and changed latex 'debug' messages to 'trace'

--- src/anima/diagnostics/logger.py
+++ src/anima/diagnostics/logger.py
@@ -1,5 +1,8 @@
+import os
 import sys
 from loguru import logger
+
+LOG_LEVEL = os.getenv("ANIMA_LOG_LEVEL", "DEBUG")
 
 # Remove default handler
 logger.remove()
@@ -8,7 +11,7 @@ logger.remove()
 logger.add(
     sys.stderr,
     format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <5}</level> | <cyan>{file.name}</cyan>: <cyan>{line: <3}</cyan> | <level>{message}</level>",
-    level="TRACE",
+    level=LOG_LEVEL,
     colorize=True,
     backtrace=True,
     diagnose=True
