ed491bec7d3eab8621a965eea24d79bd599b9d15
8bit-euclid <8bit-euclid@gmail.com>
2025-06-26 13:02:54 -0400

    chore: Some comment fixes, module init file, and neaten pyproject.toml

--- src/anima/globals/general.py
+++ src/anima/globals/general.py
@@ -8,6 +8,7 @@ from typing import Iterable
 from mathutils import Vector, Matrix, Euler
 from datetime import timedelta
 import anima.globals.easybpy as ebpy
+from anima.utils.project_utils import get_project_root
 
 
 UnitZ = Vector((0.0, 0.0, 1.0))
@@ -266,22 +267,6 @@ def save_as(file_name: str):
         file_name (str): The name of the file to save."""
     file_path = "blend/" + file_name + ".blend"
     bpy.ops.wm.save_mainfile(filepath=file_path)
-
-
-def get_project_root(marker: str = '.git') -> Path:
-    """Find the root directory of the project by looking for a 'marker' file.
-    Args:
-        marker (str): The name of the file that indicates the project root (default is '.git').
-    Returns:
-        Path: The path to the project root directory.
-    Raises:
-        FileNotFoundError: If the marker file is not found in any parent directories."""
-    current = Path(__file__).resolve()
-    for parent in current.parents:
-        if (parent/marker).exists():
-            return parent
-    raise FileNotFoundError(
-        f"Project root not found. Ensure it contains {marker}.")
 
 
 def get_pyproject_path() -> Path:
