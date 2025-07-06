2378b607236621a4010ba8fbdf714204285ce517
8bit-euclid <8bit.euclid@gmail.com>
2025-07-05 23:15:58 -0400

    style: Create option to show function info when logging

--- src/anima/diagnostics/logger.py
+++ src/anima/diagnostics/logger.py
@@ -3,6 +3,7 @@ import sys
 from loguru import logger
 
 LOG_LEVEL = os.getenv("ANIMA_LOG_LEVEL", "DEBUG")
+SHOW_FUNCTION = os.getenv("ANIMA_LOG_SHOW_FUNCTION", "0") == "1"
 
 # Remove default handler
 logger.remove()
@@ -15,17 +16,20 @@ logger.level("TRACE", color="<cyan>")
 logger.level("ERROR", color="<bold><red>")
 logger.level("CRITICAL", color="<bold><red>")
 
+
 timestamp = "<green>{time:HH:mm:ss.SSS}</green>"
 log_level = "<level>{level: <7}</level>"
+func_name = "<light-blue>{function: >26}</light-blue>" if SHOW_FUNCTION else ""
 file_name = "<green>{file.name: >25}</green>"
 line_number = "<light-green>{line: <3}</light-green>"
+file_line = file_name + ":" + line_number
 message = "<level>{message}</level>"
+format = " ".join([timestamp, log_level, func_name, file_line, message])
 
 # Add structured handler with good formatting
 logger.add(
     sys.stderr,
-    format=timestamp + " " + log_level + " " +
-    file_name + ":" + line_number + " " + message,
+    format=format,
     level=LOG_LEVEL,
     colorize=True,
     backtrace=True,
