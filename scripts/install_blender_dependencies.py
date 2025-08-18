afc70ee26afcfbedbc6ed0c12c05592c498226dd
8bit-euclid <8bit-euclid@gmail.com>
2025-08-18 04:42:39 +0000

    fix: Push file causing formatting failure

--- scripts/install_blender_dependencies.py
+++ scripts/install_blender_dependencies.py
@@ -1,8 +1,8 @@
 import subprocess
 import tomllib  # Python 3.11+ only
+
 from anima.globals.general import get_pyproject_path
 from anima.utils.blender import get_blender_root_path
-
 
 # Note: This script assumes that the blender Python path has been set in the pyproject.toml file
 
@@ -13,13 +13,11 @@ def main():
         pyproject = tomllib.load(f)
 
     bl_path = get_blender_root_path()
-    bl_python = bl_path/'4.3'/'python'/'bin'/'python3.11'
+    bl_python = bl_path / "4.3" / "python" / "bin" / "python3.11"
 
     # Ensure pip is installed and up to date
-    subprocess.run(
-        [bl_python, "-m", "ensurepip", "--upgrade"], check=True)
-    subprocess.run([bl_python, "-m", "pip", "install",
-                   "--upgrade", "pip"], check=True)
+    subprocess.run([bl_python, "-m", "ensurepip", "--upgrade"], check=True)
+    subprocess.run([bl_python, "-m", "pip", "install", "--upgrade", "pip"], check=True)
 
     # Install dependencies
     deps = pyproject["project"]["dependencies"]
